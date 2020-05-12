d1ace5d72b3be6ec82dc335f3e79d9a72e0ad4f4
aprusik <29558544+aprusik@users.noreply.github.com>
2020-05-12 13:55:00 -0400

    rebuild empty application for 2.0 remake

--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import TitleBar from 'frameless-titlebar'
-import { SoundCard } from './components/sound-card'
-import WebFont from 'webfontloader'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// Import some styles
-import './styles/app.scss'
-import { SoundList } from './components/sound-list';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-WebFont.load({
-  google: {
-    families: ['Material Icons']
-  }
-})
-
-function Element(props) {
-  return <div><h1>Hello, {props.name}!</h1></div>
-}
-
-// Create main App component
-class App extends React.Component {
-  render() {
-    return (
-      <div style={{overflow: 'hidden'}}>
-        <div style={{position: 'fixed', width: '100%', zIndex: '1'}}>
-          <TitleBar
-            app="Electron"
-            style={{position: 'fixed'}}
-            theme={{
-              barTheme: 'dark',
-              barBackgroundColor: '#1b3039'
-            }}
-          />
-        </div>
-        <div style={{position: 'relative', top: '28px', height: '100vh', overflow: 'scroll'}}>
-          <SoundList/>
-        </div>
-      </div>
-    )
-  }
-}
-
-// Render the application into the DOM, the div inside index.html
-render(<App />, document.getElementById('root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
